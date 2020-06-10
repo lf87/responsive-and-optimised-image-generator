13a27ebca6541377a9eed969db96859f2c2a35e8
lf87 <jongo1@gmail.com>
2020-06-10 22:46:49 +0100

    removed png plugin - wasn't needed

--- imagemin.js
+++ imagemin.js
@@ -1,25 +1,14 @@
-
 const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-(async () => {
-  const files = await imagemin(['build/*.{jpg,webp}'], {
-    destination: 'build/compressed',
-    plugins: [
-      imageminJpegtran({
-        quality: [0.1, 0.1]
-      }),
-      imageminPngquant({
-        quality: [0.1, 0.1]
-      })
-    ]
-  });
+const imageminMozjpeg = require('imagemin-mozjpeg');
+// const imageminPngquant = require('imagemin-pngquant');
 
+(async () => {
+  const files = await imagemin(
+    ['build/**/*.{jpg,webp}'],
+    {
+      destination: 'build/compressed/webp',
+      plugins: [imageminMozjpeg({ quality: 65 })]
+    }
+  );
   console.log(files);
-  //=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
 })();
